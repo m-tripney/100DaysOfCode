15545534b2e5a5cdbc251d5f7f11168d0240c186
Mark Tripney <mark.tripney@gmail.com>
2020-09-03 14:32:25 +0200

    Day 9 exercises, working with dicts and lists

---
+++ 3_data_structures/d9_us_states_data.py
@@ -0,0 +1,123 @@
+us_state_abbrev = {
+    "Alabama": "AL",
+    "Alaska": "AK",
+    "Arizona": "AZ",
+    "Arkansas": "AR",
+    "California": "CA",
+    "Colorado": "CO",
+    "Connecticut": "CT",
+    "Delaware": "DE",
+    "Florida": "FL",
+    "Georgia": "GA",
+    "Hawaii": "HI",
+    "Idaho": "ID",
+    "Illinois": "IL",
+    "Indiana": "IN",
+    "Iowa": "IA",
+    "Kansas": "KS",
+    "Kentucky": "KY",
+    "Louisiana": "LA",
+    "Maine": "ME",
+    "Maryland": "MD",
+    "Massachusetts": "MA",
+    "Michigan": "MI",
+    "Minnesota": "MN",
+    "Mississippi": "MS",
+    "Missouri": "MO",
+    "Montana": "MT",
+    "Nebraska": "NE",
+    "Nevada": "NV",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "New Mexico": "NM",
+    "New York": "NY",
+    "North Carolina": "NC",
+    "North Dakota": "ND",
+    "Ohio": "OH",
+    "Oklahoma": "OK",
+    "Oregon": "OR",
+    "Pennsylvania": "PA",
+    "Rhode Island": "RI",
+    "South Carolina": "SC",
+    "South Dakota": "SD",
+    "Tennessee": "TN",
+    "Texas": "TX",
+    "Utah": "UT",
+    "Vermont": "VT",
+    "Virginia": "VA",
+    "Washington": "WA",
+    "West Virginia": "WV",
+    "Wisconsin": "WI",
+    "Wyoming": "WY",
+}
+
+states = [
+    "Oklahoma",
+    "Kansas",
+    "North Carolina",
+    "Georgia",
+    "Oregon",
+    "Mississippi",
+    "Minnesota",
+    "Colorado",
+    "Alabama",
+    "Massachusetts",
+    "Arizona",
+    "Connecticut",
+    "Montana",
+    "West Virginia",
+    "Nebraska",
+    "New York",
+    "Nevada",
+    "Idaho",
+    "New Jersey",
+    "Missouri",
+    "South Carolina",
+    "Pennsylvania",
+    "Rhode Island",
+    "New Mexico",
+    "Alaska",
+    "New Hampshire",
+    "Tennessee",
+    "Washington",
+    "Indiana",
+    "Hawaii",
+    "Kentucky",
+    "Virginia",
+    "Ohio",
+    "Wisconsin",
+    "Maryland",
+    "Florida",
+    "Utah",
+    "Maine",
+    "California",
+    "Vermont",
+    "Arkansas",
+    "Wyoming",
+    "Louisiana",
+    "North Dakota",
+    "South Dakota",
+    "Texas",
+    "Illinois",
+    "Iowa",
+    "Michigan",
+    "Delaware",
+]
+
+NOT_FOUND = "N/A"
+
+
+# Perform the following tasks on the list and dict:
+# Task 1: Print 10th item in each
+for key in us_state_abbrev.keys():
+    dict_states = [key for key in us_state_abbrev.keys()]
+    dict_abbrev = [value for value in us_state_abbrev.values()]
+
+print(states[9::10])
+print(dict_states[9::10])
+
+# Task 2: Print out the 45th key in the dictionary
+print(dict_states[44])
+
+# Task 3: Print out the 27th value in the dictionary
+print(dict_abbrev[26])
